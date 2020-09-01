8b580cd3b5f2c535272256b0a9567ecd31caa409
1996toku <19960327toku@gmail.com>
2020-09-01 21:18:10 +0900

    usersテーブルの作成

--- db/migrate/20200901033753_devise_create_users.rb
+++ db/migrate/20200901033753_devise_create_users.rb
@@ -4,8 +4,16 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
   def change
     create_table :users do |t|
       ## Database authenticatable
+      t.string :name,               null: false
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
+      t.string :last_name,          null: false
+      t.string :first_name,         null: false
+      t.string :last_name_kana,     null: false
+      t.string :first_name_kana,    null: false
+      t.string :birthday,           null: false
+      t.integer :item_id,           null: false,  foreign_key: true
+      t.integer :purchses_id,        null: false,  foreign_key: true
 
       ## Recoverable
       t.string   :reset_password_token
