718f71ea42c1b9ee0d2b0402ef0e7f35d6f7db9a
1996toku <19960327toku@gmail.com>
2020-09-01 21:22:38 +0900

    トップページから新規登録、ログイン画面への遷移の実装

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,17 @@
+class UsersController < ApplicationController
+  before_action :move_to_index, except: [:index]
+
+  def index
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def move_to_index
+    unless user_signed_in?
+      redirect_to action: :index
+    end
+  end
+
+end
