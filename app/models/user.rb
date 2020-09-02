fc2cc66fab5c645be53fb791dd94125a6fc240ef
1996toku <19960327toku@gmail.com>
2020-09-02 19:24:46 +0900

    フォームの制限からバリデーションへの変更

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,7 +9,7 @@ class User < ApplicationRecord
 
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
   with_options presence: true do
-    validates :name
+    validates :name, length: { maximum: 40 }
     validates :last_name, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "full-width characters." }
     validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "full-width characters." }
     validates :last_name_kana, format: { with: /\A[ァ-ヶー－]+\z/, message: "full-width katakana characters."}
