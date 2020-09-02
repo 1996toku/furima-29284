ad4f3cdc1b03f18e207f6745d21b88663ed752e6
1996toku <19960327toku@gmail.com>
2020-09-02 19:10:36 +0900

    itemsコントローラーの作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,22 +1,2 @@
 class UsersController < ApplicationController
-  before_action :move_to_index, except: [:index]
-
-  def index
-  end
-
-  def new
-    @user = User.new
-  end
-
-  def create
-  end
-
-
-
-  def move_to_index
-    unless user_signed_in?
-      redirect_to action: :index
-    end
-  end
-
 end
