dca743cae218d1bac53d3b4e633676fbbd571f7e
1996toku <19960327toku@gmail.com>
2020-09-02 19:09:40 +0900

    バリデーションの追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,16 @@ class User < ApplicationRecord
 
   has_many :items
   has_many :purchses
+
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  with_options presence: true do
+    validates :name
+    validates :last_name, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "full-width characters." }
+    validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "full-width characters." }
+    validates :last_name_kana, format: { with: /\A[ァ-ヶー－]+\z/, message: "full-width katakana characters."}
+    validates :first_name_kana, format: { with: /\A[ァ-ヶー－]+\z/, message: "full-width katakana characters." }
+    validates :birthday
+  end
+  validates :password, format: { with: PASSWORD_REGEX, message: "Include both letters and numbers" }
+  validates :email, uniqueness: true
 end
