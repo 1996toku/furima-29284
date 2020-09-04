001c842a2f4e2e47197a0b7ebcc3e9752c4ad164
1996toku <19960327toku@gmail.com>
2020-09-04 11:06:30 +0900

    Rubocopの実行

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -5,9 +5,8 @@ class ItemsController < ApplicationController
   end
 
   private
+
   def move_to_index
-    unless user_signed_in?
-      redirect_to action: :index
-    end
+    redirect_to action: :index unless user_signed_in?
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -10,12 +10,12 @@ class User < ApplicationRecord
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
   with_options presence: true do
     validates :name, length: { maximum: 40 }
-    validates :last_name, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: "full-width characters." }
-    validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: "full-width characters." }
-    validates :last_name_kana, format: { with: /\A[ァ-ヶー－]+\z/, message: "full-width katakana characters."}
-    validates :first_name_kana, format: { with: /\A[ァ-ヶー－]+\z/, message: "full-width katakana characters." }
+    validates :last_name, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: 'full-width characters.' }
+    validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: 'full-width characters.' }
+    validates :last_name_kana, format: { with: /\A[ァ-ヶー－]+\z/, message: 'full-width katakana characters.' }
+    validates :first_name_kana, format: { with: /\A[ァ-ヶー－]+\z/, message: 'full-width katakana characters.' }
     validates :birthday
   end
-  validates :password, format: { with: PASSWORD_REGEX, message: "Include both letters and numbers" }
+  validates :password, format: { with: PASSWORD_REGEX, message: 'Include both letters and numbers' }
   validates :email, uniqueness: true
 end
