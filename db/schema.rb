7ecafa43d9f18879b62dea304c6e92471ca2156f
1996toku <19960327toku@gmail.com>
2020-09-04 16:48:55 +0900

    レビュー内容の修正2

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_01_120752) do
+ActiveRecord::Schema.define(version: 2020_09_04_064905) do
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -20,7 +20,7 @@ ActiveRecord::Schema.define(version: 2020_09_01_120752) do
     t.string "first_name", null: false
     t.string "last_name_kana", null: false
     t.string "first_name_kana", null: false
-    t.string "birthday", null: false
+    t.date "birthday", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
