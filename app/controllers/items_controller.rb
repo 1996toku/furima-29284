70356bb229b6590fa76660104d9943fc8eacd66e
1996toku <19960327toku@gmail.com>
2020-09-13 15:55:36 +0900

    商品詳細ページへの遷移

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,7 +2,10 @@ class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create]
   def index
     @items = Item.all.order("created_at DESC")
+  end
 
+  def show
+    @item = Item.find(params[:id])
   end
 
   def new
